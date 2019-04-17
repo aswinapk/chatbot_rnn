7df86af9c071e188c3406350ca9df00292ab6b5c
aswinapk <46336342+aswinapk@users.noreply.github.com>
2019-04-17 04:50:56 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,25 +1,31 @@
-from tkinter import *
-from chatbot import main,chatbot
+import random
+import tkinter as tk
+from chatbot import main, chatbot
+root = tk.Tk()
+root.title("RNN chatbot")
+root.geometry('650x400')
+user_input = tk.Entry(root)
+user_input.pack()
 
 net, sess, chars, vocab, n, beam_width, relevance, temperature, topn = main()
-root = Tk()
-user = StringVar()
-bot = StringVar()
 
-root.title(" Simple ChatBot ")
-Label(root, text=" user : ").pack(side=LEFT)
-Entry(root, textvariable=user).pack(side=LEFT)
-Label(root, text=" Bot  : ").pack(side=LEFT)
-Entry(root, textvariable=bot).pack(side=LEFT)
+greetings = ['hola', 'hello', 'hi', 'Hi', 'hey!', 'hey']
+question = ['How are you?', 'How are you doing?','what is your name?','can you help me?']
+responses = ['Okay', "I'm fine", 'My name is Chatbot','sure!, I am happy to help you']
 
 
-def cb():
-    question = user.get()
-    bot_output = chatbot(net, sess, chars, vocab, n, beam_width, relevance, temperature, topn,question)
-    # sess.close()
-    bot.set(bot_output)
+def cb(event):
+    user_text = user_input.get()
+    if user_text in greetings:
+        bot_text = random.choice(greetings)
+    elif user_text in question:
+        bot_text = responses[question.index(user_text)]
+    else:
+        bot_text = chatbot(net, sess, chars, vocab, n, beam_width, relevance, temperature, topn, user_text)
+    output.config(text=bot_text+'\n')
 
 
-Button(root, text="send", command=cb).pack(side=LEFT)
-
-mainloop()
+user_input.bind("<Return>", cb)
+output = tk.Label(root, text='\n')
+output.pack()
+tk.mainloop()
